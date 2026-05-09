c30f53f42174a1f6182def087805d22db8ea2665
saksham-2202 <sakshamd863@gmail.com>
2026-05-10 01:02:42 +0530

    pushing the file all together

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { login, logoutUser, registerUser } from "../controllers/auth.controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router();
+
+router.route("/register").post(userRegisterValidator(),validate,registerUser);
+
+router.route("/login").post(userLoginValidator(),validate,login);
+
+// access routes
+router.route("/logout").post(verifyJWT,logoutUser);
+
+export default router;
---
+++ src/validators/index.js
@@ -0,0 +1,45 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () =>{
+    return [
+        body("email")
+          .trim()
+          .notEmpty()
+          .withMessage("Email is required")
+          .isEmail()
+          .withMessage("Email is invlaid"),
+        body("username")
+          .trim()
+          .notEmpty()
+          .withMessage("Username is required")
+          .isLowercase()
+          .withMessage("Username must be in lower case")
+          .isLength({min: 3})
+          .withMessage("Username must be at least 3 characters long"),
+        body("password")
+          .trim()
+          .notEmpty()
+          .withMessage("Password is required")
+          .isEmail()
+          .withMessage("Email is invlaid"),
+        body("fullName")
+          .optional()
+          .trim()
+    ]
+}
+
+const userLoginValidator = () =>{
+    return [
+        body("email")
+            .optional()
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("password")
+            .notEmpty()
+            .withMessage("Password is required")
+    ];
+};
+
+export {
+    userRegisterValidator,userLoginValidator
+}
